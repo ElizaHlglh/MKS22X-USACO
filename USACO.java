bc849c9925755d48f36a5e075e0c39ff382ff2f1
ElizaHlglh <ewu4@stuy.edu>
2019-03-06 11:44:56 -0500

    USACO.java

--- USACO.java
+++ USACO.java
@@ -1,6 +1,13 @@
 public class USACO{
+  //bronze: take the file and read into it.
+  //1. has info on the size of land, water size, and number of trimming
+  //
   public static int bronze(String filename){
     return 1;
+  }
+
+  public int[][] bronzeH(int[][] land, int r, int c, int size){
+
   }
 
   public static int silver(String filename){
