d82ecf91cd8ac71f5133aa1aab7ac944f3cbd768
ElizaHlglh <ewu4@stuy.edu>
2019-03-07 11:41:31 -0500

    driver.java

--- USACO.java
+++ USACO.java
@@ -1,16 +1,57 @@
+import java.util.*;
+import java.io.*;
 public class USACO{
+
+  private int[][] bronzeInfo;
+  private ArrayList<String> brLines;
+
   //bronze: take the file and read into it.
   //1. has info on the size of land, water size, and number of trimming
-  //
-  public static int bronze(String filename){
+  //2. 6x6 ft is size of area
+  public static int bronze(String filename) throws FileNotFoundException{
+    ArrayList<String> brLines = new ArrayList<String>();
+    File file = new File(filename);
+    Scanner info = new Scanner(file);
+    while(info.hasNextLine()){
+      brLines.add(info.nextLine());
+    }
     return 1;
   }
 
-  public int[][] bronzeH(int[][] land, int r, int c, int size){
+  //public int[][] bronzeH(int[][] land, int r, int c, int size){
+//help to shrink/trim the land
+  //}
 
+  public static ArrayList<Integer> bronzeBreakStr(String info){ //given a string, return an array of int in it
+    int intcount = 0;
+    for (int i = 0; i < info.length(); i++){
+      if (info.charAt(i) == ' '){
+        intcount++;
+      }
+    }
+    ArrayList<Integer> ans = new ArrayList<Integer>();
+    int previousSpace = 0;
+    for (int i = 0; i < info.length(); i++){
+      if (info.charAt(i) == ' '){
+        if (previousSpace == 0){
+          ans.add(Integer.parseInt(info.substring(previousSpace, i)));
+          previousSpace = i;
+        }
+        else{
+          ans.add(Integer.parseInt(info.substring(previousSpace+1, i)));
+          previousSpace = i;
+        }
+      }
+    }
+    ans.add(Integer.parseInt(info.substring(previousSpace+1,info.length())));
+    return ans;
   }
 
-  public static int silver(String filename){
+  /*public static int silver(String filename){
+//how many ways in 6 steps/seconds can reach final spot
+//can't return to previous spots
+//planning: create a second board to show possible location and place (-) on previous spot
+    //try catch Scanner
     return 1;
-  }
+  }*/
 }
---
+++ driver.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class driver{
+    public static void main(String[]args){
+      String test1 = "4 6 22 2";
+      System.out.println(test1);
+      System.out.println("Testing int extraction: ");
+      System.out.println(USACO.bronzeBreakStr(test1));
+    }
+}
