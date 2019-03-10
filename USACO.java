512228488263c5ef7dbebc5f5b82c9c0993814b2
ElizaHlglh <ewu4@stuy.edu>
2019-03-10 15:09:30 -0400

    driver.java

--- USACO.java
+++ USACO.java
@@ -15,72 +15,94 @@ public class USACO{
     while(info.hasNextLine()){ //store info of every line
       brLines.add(info.nextLine());
     }
-    System.out.println(brLines);
-    ArrayList<Integer> firstLine = bronzeBreakStr(brLines.get(0));
-    int[][] map = new int[firstLine.get(0)][firstLine.get(1)];
+    //System.out.println(brLines);
+    ArrayList<Integer> firstLine = bronzeBreakStr(brLines.get(0));//get the first line with the info of the map size, repeatation, and elevation
+    int[][] map = new int[firstLine.get(0)][firstLine.get(1)]; //create map with the size from line 1
     for (int r = 1; r < brLines.size() - firstLine.get(3); r++){ //create the map by getting the row number and just add each int from brLines to the map
       ArrayList<Integer> currentLine = bronzeBreakStr(brLines.get(r));
-      System.out.println(currentLine);
+      //System.out.println(currentLine);
       for (int c = 0; c < currentLine.size(); c++){
         map[r-1][c] = currentLine.get(c);
       }
     }
     //testing purpose
-    for (int row = 0; row < map.length; row++){
+    /*for (int row = 0; row < map.length; row++){
       for (int col = 0; col < map[row].length; col++){
         System.out.print(map[row][col] + " ");
       }
       System.out.println();
     }
-    System.out.println();
+    System.out.println();*/
 
     //start trimming the land:
     //check every number of command/changes
     //from the end of the map info to the end of the
+    //System.out.println("map.length : " + map.length);
     for (int ch = map.length+1; ch < brLines.size(); ch++){
       ArrayList<Integer> thisLine = bronzeBreakStr(brLines.get(ch));
-      System.out.println("ThisLine testing : " + thisLine);
+      //System.out.println("ThisLine testing : " + thisLine);
       map = bronzeH(map, thisLine.get(0), thisLine.get(1), thisLine.get(2));
-      for (int row = 0; row < map.length; row++){
-        for (int col = 0; col < map[row].length; col++){
-          System.out.print(map[row][col] + " ");
+    }
+    /*for (int row = 0; row < map.length; row++){
+      for (int col = 0; col < map[row].length; col++){
+        System.out.print(map[row][col] + " ");
+      }
+      System.out.println();
+    }*/
+    int lakeSize = 0;
+    //compare the water level with the map
+    //find area with less than water level and then use elevation number to subtract the lower land size to find lake size
+    for (int row = 0; row < map.length; row++){
+      for (int col = 0; col < map[row].length; col++){
+        if (map[row][col] < firstLine.get(2)){
+          map[row][col] = firstLine.get(2) - map[row][col];
+          lakeSize += map[row][col];
         }
-        System.out.println();
+        else{
+          map[row][col] = -1;
+        }
       }
     }
+    /*System.out.println("lakeSize: " + lakeSize); //print map
     for (int row = 0; row < map.length; row++){
       for (int col = 0; col < map[row].length; col++){
         System.out.print(map[row][col] + " ");
       }
       System.out.println();
-    }
-    return 1;
+    }*/
+    return lakeSize * 72 * 72;//for volume, multiple the height by the width and length(6x6 ft/ 72x72 inch)
   }
 
   public static int[][] bronzeH(int[][] land, int r, int c, int size){
 //help to shrink/trim the land
     //find the largest land size first
+    //System.out.println("r : " + r + "       c : " + c + "        size : " + size);
     ArrayList<Integer> height = new ArrayList<Integer>();
     int[] moveR = {-1,-1,-1, 0,0,0, 1,1,1};
     int[] moveC = {-1, 0, 1,-1,0,1,-1,0,1};
     for (int i = 0; i<moveR.length; i++){ //collects the height of the 3x3 grid
       if ((r+moveR[i] >= 0 && r+moveR[i] < land.length) && (c+moveC[i] >= 0 && c+moveC[i] < land[0].length)){
         height.add(land[r+moveR[i]][c+moveC[i]]);
+        //System.out.println("Coordinates: " + r+moveR[i] + ", " + c+moveC[i]);
       }
     }
+  //  System.out.println("height is: " + height);
     Collections.sort(height); //sort the height
-    System.out.println("Height : " + height);
+    //System.out.println("heightA is: " + height);
     int after = 0;
     for (int z = 0; z<moveR.length; z++){ //modify the largest height
-      if (land[r+moveR[z]][r+moveC[z]] == height.get(height.size()-1)){
-        land[r+moveR[z]][r+moveC[z]] -= size;
-        after = land[r+moveR[z]][r+moveC[z]];
+      if (land[r+moveR[z]][c+moveC[z]] == height.get(height.size()-1)){
+        //System.out.println("land to trim: " + land[r+moveR[z]][c+moveC[z]]);
+        land[r+moveR[z]][c+moveC[z]] =  land[r+moveR[z]][c+moveC[z]] - size;
+        //System.out.println("Trimming : " + land[r+moveR[z]][r+moveC[z]]);
+        after = land[r+moveR[z]][c+moveC[z]];
       }
     }
+    //System.out.println("After : " + after);
 
-    for (int z = 0; z<moveR.length; z++){ //modify the other heights to be the same as the largest after trimming
-      if (land[r+moveR[z]][r+moveC[z]] >= after){
-        land[r+moveR[z]][r+moveC[z]] = after;
+    for (int j = 0; j<moveR.length; j++){ //modify the other heights to be the same as the largest after trimming
+      if (land[r+moveR[j]][c+moveC[j]] >= after){
+        land[r+moveR[j]][c+moveC[j]] = after;
       }
     }
     return land;
--- driver.java
+++ driver.java
@@ -1,4 +1,4 @@
-import java.io.File;
+/*import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class driver{
@@ -14,4 +14,39 @@ public class driver{
       System.out.println("Invalid filename: ");
     }
   }
+}*/
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+//DRIVER BORROWED FROM GOOGLE GROUP, CREDITS TO: Bryant Stringham and Derek Lao
+public class driver {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Bronze:");
+            for (int i = 1; i < 6; i++) {
+                File correct = new File("testCases/makelake." + i + ".out");
+                Scanner yes = new Scanner(correct);
+                int yourAnswer = USACO.bronze("testCases/makelake." + i + ".in");
+                int correctAnswer = yes.nextInt();
+                System.out.println(i + " Yours: " + yourAnswer + " Key: " + correctAnswer + " " + (yourAnswer == correctAnswer));
+                yes.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        /*try {
+            //System.out.println(USACO.silver("testCases/ctravel.2.in"));
+            System.out.println("Silver:");
+            for (int i = 1; i < 6; i++) {
+                File checkPlus = new File("testCases/ctravel." + i + ".out");
+                Scanner A = new Scanner(checkPlus);
+                int yourAnswer = USACO.silver("testCases/makelake." + i + ".in");
+                int correctAnswer = A.nextInt();
+                System.out.println(i + " Yours: " + yourAnswer + " Key: " + correctAnswer + " " + (yourAnswer == correctAnswer));
+                A.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }*/
+    }
 }
